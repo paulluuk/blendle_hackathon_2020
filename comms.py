82b9906ba8d1360defa6aa21575e28603a72e342
Pluk <meneerpluk@gmail.com>
2020-03-06 16:58:05 +0100

    a few more comments

--- comms.py
+++ comms.py
@@ -3,27 +3,34 @@ from subprocess import Popen, PIPE
 
 class InteractiveSession:
     def __init__(self, python_or_ruby, path):
+        # this subprocess is pretty shake, it will
+        # get stuck if the subprocess does not behave
+        # exactly as expected.
         self.process = self.start([python_or_ruby, path])
 
-    def start(self, executable_file):
+    def start(self, execute_cmd):
+        # start a new subprocess
         return Popen(
-            executable_file,
+            execute_cmd,
             stdin=PIPE,
             stdout=PIPE,
             stderr=PIPE
         )
 
     def read(self):
+        # read from the subprocess' output
         response = self.process.stdout.readline().decode("utf-8").strip()
         print("(output) {}".format(response))
         return response
 
     def write(self, message):
+        # write to the subprocess' input
         print("(input)  {}".format(message))
         self.process.stdin.write(f"{message.strip()}\n".encode("utf-8"))
         self.process.stdin.flush()
 
     def terminate(self):
+        # close the subprocess
         self.process.stdin.close()
         self.process.terminate()
         self.process.wait(timeout=0.2)
--- run.py
+++ run.py
@@ -2,7 +2,11 @@ import glob, os, random
 
 import game, fighter
 
+# get a list of available fighters
 fighters = glob.glob(os.path.join("fighters", "*"))
+# create the left fighter
 f1 = fighter.Fighter(random.choice(fighters))
+# create the right fighter
 f2 = fighter.Fighter(random.choice(fighters))
+# launch the game
 game.Game(f1, f2).process()

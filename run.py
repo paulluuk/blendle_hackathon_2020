f058c25851866a96c50afd1ec906b7ff2f7ba3d9
Paul-Luuk Profijt <meneerpluk@gmail.com>
2020-03-06 18:37:47 +0100

    let all fighters fight eachother

--- run.py
+++ run.py
@@ -4,9 +4,13 @@ import game, fighter
 
 # get a list of available fighters
 fighters = glob.glob(os.path.join("fighters", "*"))
-# create the left fighter
-f1 = fighter.Fighter(random.choice(fighters))
-# create the right fighter
-f2 = fighter.Fighter(random.choice(fighters))
-# launch the game
-game.Game(f1, f2).process()
+
+# create a fighting roster
+for fighter_path1 in fighters:
+    for fighter_path2 in fighters:
+        # create the left fighter
+        f1 = fighter.Fighter(fighter_path1)
+        # create the right fighter
+        f2 = fighter.Fighter(fighter_path2)
+        # launch the game
+        game.Game(f1, f2).process()
